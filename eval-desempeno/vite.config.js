3751b3909c70a36778f1eab817403c2b5d68e527
Andrea Vargas <andreavargaspsic@gmail.com>
2026-05-12 15:21:41 -0400

    Delete eval-desempeno directory

--- eval-desempeno/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-const REPO_NAME = '/Primeraevaluacion/'
-
-export default defineConfig({
-  plugins: [react()],
-  base: REPO_NAME,
-})
